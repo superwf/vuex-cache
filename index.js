621fcbbdada6fe979c33497097ca8f9d36870725
superwf <superwf@gmail.com>
2017-03-22 20:10:03 +0800

    update to 0.3.0

--- index.js
+++ index.js
@@ -11,13 +11,24 @@ var index = (function (store) {
     return cache[type];
   };
 
-  store.clearCache = function () {
+  store.removeCache = function () {
     var type = arguments.length <= 0 ? undefined : arguments[0];
     if (type in cache) {
       delete cache[type];
       return true;
     }
     return false;
+  };
+
+  store.hasCache = function (key) {
+    return key in cache;
+  };
+
+  store.clearCache = function () {
+    for (var key in cache) {
+      delete cache[key];
+    }
+    return true;
   };
 });
 
